0bc2b7f3a29818854e3a1185d893f1a614290059
Fadhel <fadhelmr1710@gmail.com>
2025-10-31 14:34:23 +0700

    update responsive about page

--- assets/js/about.js
+++ assets/js/about.js
@@ -7,3 +7,11 @@ window.addEventListener("scroll", () => {
     header.style.background = "transparent";
   }
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
